530b766fa92d5062372e75748c11e61ed9105698
TinaChen071 <126639752+TinaChen071@users.noreply.github.com>
2023-03-08 09:07:34 +0800

    style: change color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'green': '#B3CC59',
+      'mint': '#E6EBD0',
+      'yellow': '#FFF8DC',
+    }
   },
   plugins: [],
 }
